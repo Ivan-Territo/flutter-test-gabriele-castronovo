7d7762799de6663e4964ad9761a06e15b206135a
Your Name <you@example.com>
2026-02-11 11:24:35 +0100

    iniziale

--- lib/models/room.dart
+++ lib/models/room.dart
@@ -1,7 +1,24 @@
 class Room {
 
-  Room() {
-  }
+  final int id;
+  final String? nomeStanza;
+  final String? tipologia;
+  final int? prezzoNotte;
+  final int? postiLetto;
+  final List<String>? servizi;
+  final bool? disponibile;
+  final String? urlImmagine;
+
+  Room({
+    required this.id,
+    this.nomeStanza,
+    this.tipologia,
+    this.prezzoNotte,
+    this.postiLetto,
+    this.servizi,
+    this.disponibile,
+    this.urlImmagine
+  });
 
   factory Room.fromJson(Map<String, dynamic> json) {
     return Room(
